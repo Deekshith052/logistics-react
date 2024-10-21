e07a14c7d2e20e59bedb6357407b066c41b8066f
Deekshith052 <deekshithrgowda20022gmail.com>
2024-10-21 16:41:44 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogisticsDashboard from './components/LogisticsDashboard';
 
-function App() {
+
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LogisticsDashboard />
     </div>
   );
-}
+};
 
 export default App;
